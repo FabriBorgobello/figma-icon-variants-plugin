456102eab91e5317ede84e72c8e956238bd39f60
Fabricio Borgobello <fborgobello@gmail.com>
2024-02-06 16:56:10 +0100

    Readme

--- README.md
+++ README.md
@@ -1,40 +1,21 @@
-Below are the steps to get your plugin running. You can also find instructions at:
+# Figma Icon Variants Creator Plugin
 
-  https://www.figma.com/plugin-docs/plugin-quickstart-guide/
+This Figma plugin allows designers to efficiently create multiple size variants of icon components directly within Figma.
 
-This plugin template uses Typescript and NPM, two standard tools in creating JavaScript applications.
+## Installation
 
-First, download Node.js which comes with NPM. This will allow you to install TypeScript and other
-libraries. You can find the download link here:
+1. Clone the plugin repository to your local machine.
+2. Add the plugin from Figma context menu.
 
-  https://nodejs.org/en/download/
+## Usage
 
-Next, install TypeScript using the command:
+1. Select the frame containing the icons.
+2. Complete the form with the desired icon names and their respective variants.
+3. Click on the "Start" button.
 
-  npm install -g typescript
+## Development
 
-Finally, in the directory of your plugin, get the latest type definitions for the plugin API by running:
-
-  npm install --save-dev @figma/plugin-typings
-
-If you are familiar with JavaScript, TypeScript will look very familiar. In fact, valid JavaScript code
-is already valid Typescript code.
-
-TypeScript adds type annotations to variables. This allows code editors such as Visual Studio Code
-to provide information about the Figma API while you are writing code, as well as help catch bugs
-you previously didn't notice.
-
-For more information, visit https://www.typescriptlang.org/
-
-Using TypeScript requires a compiler to convert TypeScript (code.ts) into JavaScript (code.js)
-for the browser to run.
-
-We recommend writing TypeScript code using Visual Studio code:
-
-1. Download Visual Studio Code if you haven't already: https://code.visualstudio.com/.
-2. Open this directory in Visual Studio Code.
-3. Compile TypeScript to JavaScript: Run the "Terminal > Run Build Task..." menu item,
-    then select "npm: watch". You will have to do this again every time
-    you reopen Visual Studio Code.
-
-That's it! Visual Studio Code will regenerate the JavaScript file every time you save.
+1. Install the dependencies with `npm install`.
+2. Update the `code.ts` file.
+3. Build the plugin with `npm run build` (this will create a `code.js` file).
+4. Add the new build to Figma.
---
+++ code.js
@@ -0,0 +1,97 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+figma.showUI(__html__, { width: 500, height: 500 });
+figma.ui.onmessage = (msg) => {
+    if (msg.type === 'cancel')
+        figma.closePlugin();
+    if (msg.type === 'create-variants') {
+        createIconComponents(msg.payload);
+    }
+};
+function createIconComponents(variants = []) {
+    return __awaiter(this, void 0, void 0, function* () {
+        figma.notify('Creating variants, please wait...');
+        // Get frame elements inside selection.
+        const frame = figma.currentPage.selection[0];
+        if (!frame || frame.type !== 'FRAME') {
+            figma.notify('Please select a frame to create variants.');
+            figma.closePlugin();
+            return;
+        }
+        // Create frame for output.
+        const outputFrame = createOutputFrame();
+        // Create component variants.
+        createComponentVariants(frame, outputFrame, variants);
+        figma.notify('Variants created successfully.');
+        figma.closePlugin();
+    });
+}
+function createOutputFrame() {
+    const outputFrame = figma.createFrame();
+    outputFrame.name = 'Components ' + new Date().toISOString();
+    outputFrame.layoutMode = 'HORIZONTAL';
+    outputFrame.itemSpacing = 100;
+    outputFrame.primaryAxisSizingMode = 'AUTO';
+    outputFrame.counterAxisSizingMode = 'AUTO';
+    outputFrame.verticalPadding = 100;
+    outputFrame.horizontalPadding = 100;
+    return outputFrame;
+}
+function createComponentVariants(frame, outputFrame, variants) {
+    for (const icon of frame.children) {
+        if (icon.type !== 'FRAME')
+            continue;
+        // Create component variants.
+        const components = variants.map((variant) => {
+            const clone = icon.clone();
+            const component = figma.createComponent();
+            component.name = `size=${variant.name}`;
+            component.resizeWithoutConstraints(variant.value, variant.value);
+            component.appendChild(clone);
+            clone.x = 0;
+            clone.y = component.height;
+            resizeAndCenter(clone, variant.value);
+            figma.ungroup(clone);
+            return component;
+        });
+        // Create component set.
+        const componentSet = figma.combineAsVariants(components, outputFrame);
+        componentSet.name = icon.name;
+        componentSet.layoutMode = 'VERTICAL';
+        componentSet.counterAxisAlignItems = 'CENTER';
+        componentSet.itemSpacing = 50;
+    }
+}
+function resizeAndCenter(icon, variantValue) {
+    // For each frame, take name, width and height.
+    const { width, height } = icon;
+    // Take the highest value and expand all frames to that value. (resizing the other dimension proportionally)
+    if (width > height) {
+        icon.resize(variantValue, height * (variantValue / width));
+    }
+    else {
+        icon.resize(width * (variantValue / height), variantValue);
+    }
+    // Expand the lowest value to the variant value without resizing the other dimension.
+    if (width < height) {
+        icon.resizeWithoutConstraints(variantValue, variantValue);
+    }
+    else {
+        icon.resizeWithoutConstraints(variantValue, variantValue);
+    }
+    // Get all icon's children, group them and center them inside the frame.
+    // (this is necessary to center all the vectors and not each one individually)
+    const childrenGroup = figma.group(icon.children, icon);
+    childrenGroup.x = icon.width / 2 - childrenGroup.width / 2;
+    childrenGroup.y = icon.height / 2 - childrenGroup.height / 2;
+    // Un-group the children.
+    figma.ungroup(childrenGroup);
+}
